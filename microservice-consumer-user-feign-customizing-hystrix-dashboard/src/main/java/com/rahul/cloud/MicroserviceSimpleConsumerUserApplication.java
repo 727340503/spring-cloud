3a029697edb0265b0ecfbe65a86b22cc3e1d9df1
Rahul Rui <rahulrui@ideaculture.com>
2017-11-21 11:19:03 +0800

    add hystrix dashboard demo

--- microservice-consumer-user-feign-customizing-hystrix-dashboard/src/main/java/com/rahul/cloud/MicroserviceSimpleConsumerUserApplication.java
+++ microservice-consumer-user-feign-customizing-hystrix-dashboard/src/main/java/com/rahul/cloud/MicroserviceSimpleConsumerUserApplication.java
@@ -3,15 +3,13 @@ package com.rahul.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
-@EnableHystrix
+@EnableHystrixDashboard
 public class MicroserviceSimpleConsumerUserApplication {
 	
 	@Bean
